08c3e5cb44fc5bf9818401136f49bbe3ef968c77
Crowdbotics <engineering@crowdbotics.com>
2021-09-28 20:57:47 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -8,7 +8,7 @@ export const globalOptions = {
 
 export const modulesOptions = {
   "@modules/app-menu": {
-    title: "App Menu123",
+    title: "XX",
     copy: "Routes available!",
     styles: styles
   }
